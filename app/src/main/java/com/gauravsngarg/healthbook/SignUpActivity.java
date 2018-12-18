07444c5feeb876d67d2c233777ca4f9e1ce79c50
Gaurav Garg <gauravsngarg@gmail.com>
2018-12-18 08:38:29 +0530

    Add Login, Signup Activity
    Integrate Authentication with Firebase

--- app/src/main/java/com/gauravsngarg/healthbook/SignUpActivity.java
+++ app/src/main/java/com/gauravsngarg/healthbook/SignUpActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -18,63 +17,53 @@ import com.google.firebase.auth.FirebaseAuth;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LogInActivity extends AppCompatActivity {
-
-    @BindView(R.id.emailField)
-    protected EditText emailEditText;
+public class SignUpActivity extends AppCompatActivity {
     @BindView(R.id.passwordField)
     protected EditText passwordEditText;
-    @BindView(R.id.loginButton)
-    protected Button logInButton;
-    @BindView(R.id.signUpText)
-    protected TextView signUpTextView;
+    @BindView(R.id.emailField)
+    protected EditText emailEditText;
+    @BindView(R.id.signupButton)
+    protected Button signUpButton;
 
     private FirebaseAuth mFirebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_log_in);
-        mFirebaseAuth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_sign_up);
 
         ButterKnife.bind(this);
+        // Initialize FirebaseAuth
+        mFirebaseAuth = FirebaseAuth.getInstance();
 
-        signUpTextView.setOnClickListener(new View.OnClickListener() {
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LogInActivity.this, SignUpActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        logInButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
+                String email = emailEditText.getText().toString();
 
-                email = email.trim();
                 password = password.trim();
+                email = email.trim();
 
-                if (email.isEmpty() || password.isEmpty()) {
-                    AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
-                    builder.setMessage(R.string.login_error_message)
-                            .setTitle(R.string.login_error_title)
+                if (password.isEmpty() || email.isEmpty()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                    builder.setMessage(R.string.signup_error_message)
+                            .setTitle(R.string.signup_error_title)
                             .setPositiveButton(android.R.string.ok, null);
                     AlertDialog dialog = builder.create();
                     dialog.show();
                 } else {
-                    mFirebaseAuth.signInWithEmailAndPassword(email, password)
-                            .addOnCompleteListener(LogInActivity.this, new OnCompleteListener<AuthResult>() {
+                    mFirebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                                 @Override
                                 public void onComplete(@NonNull Task<AuthResult> task) {
                                     if (task.isSuccessful()) {
-                                        Intent intent = new Intent(LogInActivity.this, MainActivity.class);
+                                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(intent);
                                     } else {
-                                        AlertDialog.Builder builder = new AlertDialog.Builder(LogInActivity.this);
+                                        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
                                         builder.setMessage(task.getException().getMessage())
                                                 .setTitle(R.string.login_error_title)
                                                 .setPositiveButton(android.R.string.ok, null);
@@ -86,8 +75,6 @@ public class LogInActivity extends AppCompatActivity {
                 }
             }
         });
-
-
     }
 
 }
