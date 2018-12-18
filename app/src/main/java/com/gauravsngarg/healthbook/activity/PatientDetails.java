2359b742b8fa5c5a19d04b22b8fa84af2904d455
Gaurav Garg <gauravsngarg@gmail.com>
2018-12-18 19:24:43 +0530

    Add Patient Activity

---
+++ app/src/main/java/com/gauravsngarg/healthbook/activity/PatientDetails.java
@@ -0,0 +1,15 @@
+package com.gauravsngarg.healthbook.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gauravsngarg.healthbook.R;
+
+public class PatientDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_details);
+    }
+}
