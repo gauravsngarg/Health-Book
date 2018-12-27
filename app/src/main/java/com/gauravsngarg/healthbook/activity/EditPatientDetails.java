287d1b8ceaa261563172062f8a4e25a1ac5d50be
Gaurav Garg <gauravsngarg@gmail.com>
2018-12-27 20:00:45 +0530

    Merge with other basic commits

---
+++ app/src/main/java/com/gauravsngarg/healthbook/activity/EditPatientDetails.java
@@ -0,0 +1,15 @@
+package com.gauravsngarg.healthbook.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.gauravsngarg.healthbook.R;
+
+public class EditPatientDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_patient_details);
+    }
+}
